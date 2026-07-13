4f11dec8d6bda60db1299c80471dd6a78d2eed1b
Bran <fanxiaoboom@gmail.com>
2026-07-13 11:04:58 +0800

    Fix O-mos import ordering

--- components/PageAgentWindow.tsx
+++ components/PageAgentWindow.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { useEffect, useRef, useState } from 'react'
 import type { FormEvent, PointerEvent } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import { SparkleIcon, XIcon } from '~/assets'
 
